7bc9a938243d2214b93ecc6d68bc7075078f4e8e
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2023-05-10 13:38:42 +0200

    ci: add debug info

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,21 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-node';
 
+console.log('PORT is set to ', process.env?.PORT);
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// these are default values anyway, get port from ENV
+			env: {
+				//host: 'HOST',
+				port: 'PORT'
+			}
+		})
 	}
 };
 
